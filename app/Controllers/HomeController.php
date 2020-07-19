9c32846e65ef10f44246b13818205afad561ca5b
Moose <matt_salmon@hotmail.com>
2020-07-19 17:30:46 -0500

    Added variables for wolf_order, nassau, adjusted template for register and hole layout.

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -24,16 +24,57 @@ class HomeController extends Controller
     {
         $args = func_get_arg(1);
         $playerCount = $args['player_count'] * 1;
-        $playerSkins = (float) $args['player_skins'] * 1.0;
+        $skinsValue = (float) $args['skins_value'] * 1.0;
+        $golfCourse = $args['course_name'];
+
+        $currentHole = $params['current_hole'] ?: null;
+        $matchHoles = $params['match_holes'] ?: null;
+        $availableSkins = $params['available_skins'] ?: null; //counter for available skins, holds carry over.
+
+        // added variables
+        $playerName = $args['player_name'];
+        $playerWolfOrder = $args['player_wolf_order'];
+        $playerNassau = $args['player_nassau'];
+        $playerHoleScore = $args['player_hole_score'];
+        $playerSkins = $args['player_skins'];
+        $playerTotalScore = $args['player_total_score'];
+        $wolfPartner = $args['wolf_partner'];
+
 
         $this->setSessionArgs('playerCount', $playerCount);
+        $this->setSessionArgs('golfCourse', $golfCourse);
+        $this->setSessionArgs('matchSkins', $matchSkins);
+
+        $this->setSessionArgs('playerName', $playerName);
+        $this->setSessionArgs('playerWolfOrder', $playerWolfOrder);
+        $this->setSessionArgs('playerNassau', $playerNassau);
+        $this->setSessionArgs('playerHoleScore', $playerHoleScore);
         $this->setSessionArgs('playerSkins', $playerSkins);
+        $this->setSessionArgs('playerTotalScore', $playerTotalScore);
+        $this->setSessionArgs('wolfPartner', $wolfPartner);
 
         $params = [
           'data' => [
                 'playerCount' => $playerCount,
-                'playerSkins' => $playerSkins,
+                'golfCourse' => $golfCourse,
+                'skinsValue' => $skinsValue,
                 'playerTerms' => $args['player_terms'] ?: "off",
+                'currentHole' => $currentHole,
+                // added array
+                'matchHoles' => $matchHoles,
+                'player_data' => [
+                    'player_number' => [
+                        'playerName' => $playerName,
+                        'playerWolfOrder' => $playerWolfOrder,
+                        'playerNassau' => $playerNassau,
+                        'playerHoleScore' => $playerHoleScore,
+                        'playerSkins' => $playerSkins,
+                        'playerTotalScore' => $playerTotalScore,
+                        'wolfPartner' => $wolfPartner,
+                        'matchSkins' => $matchSkins, // match skins added here to assist with debugging later.
+                        'matchHoles' => $matchHoles,
+                      ],
+                  ],
             ],
         ];
 
--- app/Models/GameStartDTO.php
+++ app/Models/GameStartDTO.php
@@ -6,14 +6,47 @@ namespace DiscWolf\Models;
 
 final class GameStartDTO
 {
+    // Single value match variables.
     private ?int $playerCount;
-    private ?float $skinsPerHole;
+    private ?float $skinsValue;
+    private ?string $golfCourse;
     private ?bool $agreedToTerms;
+
+    // Multi-value and adjustable match variables.
+    private ?int $currentHole:
+    private ?int $matchHoles;
+    private ?int $availableSkins;
+
+    // Player values
+    private ?string $playerName;
+    private ?int $playerWolfOrder;
+    private ?bool $playerNassau;
+    private ?int $playerHoleScore;
+    private ?int $matchScore;
+    private ?string $holePartner;
+    private ?int $playerSkins;
+    private ?int $playerTotalScore;
 
     public function __construct(array $params = [])
     {
+        // Single value match variables.
         $this->playerCount = $params['playerCount'] ?: null;
-        $this->skinsPerHole = $params['playerSkins'] ?: null;
+        $this->golfCourse = $params['golfCourse'] ?: null;
+        $this->skinsValue = $params['skinsValue'] ?: null;
         $this->agreedToTerms = $params['playerTerms'] ?: null;
+
+        // Multi-value and adjustable match variables.
+        $this->$currentHole = $params['currentHole'] ?: null;
+        $this->$matchHoles = $params['matchHoles'] ?: null;
+        $this->availableSkins = $params['availableSkins'] ?: null; //counter for available skins, holds carry over.
+
+        // Player values
+        $this->playerName = $params['playerName'] ?: null;
+        $this->playerWolfOrder = $params['playerWolfOrder'] ?: null;
+        $this->playerNassau = $params['playerNaussau'] ?: null;
+        $this->playerHoleScore = $params['playerHoleScore'] ?: null;
+        $this->playerSkins = $params['playerSkins'] ?: null;
+        $this->playerTotalScore = $params['playerTotalScore'] ?: null;
+        $this->wolfPartner = $params['wolfPartner'] ?: null; //only wolf team will have partners, all others will be 0.
     }
 }
